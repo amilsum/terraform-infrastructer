c2b79ddc33e9e40b73f18827c59c9e8c90073474
amilsum <137114831+amilsum@users.noreply.github.com>
2023-09-23 11:45:05 -0500

    adedd subnet group

--- ec2/main.tf
+++ ec2/main.tf
@@ -27,8 +27,8 @@ resource "aws_instance" "web" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
